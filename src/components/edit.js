f6709f3b3267b9ddf8d870274dbf339f705da986
Rasik <rasikraj01@gmail.com>
2018-01-14 23:56:43 +0530

    inputs

--- src/components/edit.js
+++ src/components/edit.js
@@ -2,11 +2,39 @@ import React, { Component } from 'react';
 import './../style/edit.css';
 
 class Edit extends Component {
+   ValueonChange(value){
+      console.log(value);
+   }
   render() {
     return (
       <div className="Edit">
-         <h1>Edit</h1>
-      </div>
+         <h1>EDIT</h1>
+<div className="sliders">
+   <div className="warp">
+<p>Hue:</p>
+<input type="range" min="1" max="100" className="slider" id="myRange"/>
+<p>Hue:</p>
+<input type="range" min="1" max="100" className="slider" id="myRange"/>
+<p>Hue:</p>
+<input type="range" min="1" max="100" className="slider" id="myRange"/>
+<p>Hue:</p>
+<input type="range" min="1" max="100" className="slider" id="myRange"/>
+<p>Hue:</p>
+<input type="range" min="1" max="100" className="slider" id="myRange"/>
+<p>Hue:</p>
+<input type="range" min="1" max="100" className="slider" id="myRange"/>
+<p>Hue:</p>
+<input type="range" min="1" max="100" className="slider" id="myRange"/>
+<p>Hue:</p>
+<input type="range" min="1" max="100" className="slider" id="myRange"/>
+<p>Hue:</p>
+<input type="range" min="1" max="100" className="slider" id="myRange"/>
+<p>Hue:</p>
+<input type="range" min="1" max="100" className="slider" id="myRange"/>
+<p>Hue:</p>
+<input type="range" min="1" max="100" className="slider" id="myRange"/>
+</div></div></div>
+
     );
   }
 }
