9db0ab261c09cf12361258049e9d838ab645848d
Rasik <rasikraj01@gmail.com>
2018-02-10 00:04:18 +0530

    add image

--- src/components/AddImagetoEdit.js
+++ src/components/AddImagetoEdit.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import './../style/uploadImage.css';
+import $ from 'jquery';
 
 class AddImagetoEdit extends Component {
    onFormSubmit(e){
       e.preventDefault();
-      console.log('got url');
+      var imageURL = $('#image_url').val();
+
    }
   render() {
     return (
       <div className="AddImagetoEdit">
-            <input name="url" type="text" placeholder="enter url to edit image"/>
+            <input id="image_url" name="url" type="text" placeholder="enter url to edit image"/>
             <input type="submit" onClick={this.onFormSubmit}/>
       </div>
     );
