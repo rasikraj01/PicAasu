7592e2497b06d8e6fbd4f35f5d56ddba64ed0a8b
Rasik <rasikraj01@gmail.com>
2018-02-10 15:54:40 +0530

    update images feature

--- src/components/AddImagetoEdit.js
+++ src/components/AddImagetoEdit.js
@@ -3,16 +3,17 @@ import './../style/uploadImage.css';
 import $ from 'jquery';
 
 class AddImagetoEdit extends Component {
-   onFormSubmit(e){
-      e.preventDefault();
-      var imageURL = $('#image_url').val();
-
-   }
+   // onFormSubmit(e){
+   //    e.preventDefault();
+   //    var imageURL = $('#image_url').val();
+   //    console.log(imageURL);
+   // }
   render() {
+
     return (
       <div className="AddImagetoEdit">
-            <input id="image_url" name="url" type="text" placeholder="enter url to edit image"/>
-            <input type="submit" onClick={this.onFormSubmit}/>
+            <input id="image_url" name="url" type="text" placeholder="enter url to edit image" />
+            <input type="submit" onClick={this.props.UpdateImage}/>
       </div>
     );
   }
--- src/components/uploadImage.js
+++ src/components/uploadImage.js
@@ -4,14 +4,33 @@ import Image from './image.js';
 import toEdit from './../imgs/test.jpg';
 import SaveImage from './saveImage';
 import AddImagetoEdit from './AddImagetoEdit';
+import $ from 'jquery';
+
 
 
 class UploadImage extends Component {
+
+   constructor(props){
+      super(props);
+      this.state = {
+            url : toEdit
+      }
+      this.UpdateImage = this.UpdateImage.bind(this);
+   }
+
+
+   UpdateImage(){
+      var url = $('#image_url').val();
+      //console.log(url);
+      this.setState({url});
+      $('#image_url').val('');
+   }
+
   render() {
     return (
       <div className="uploadImage">
-         <Image imgSource={toEdit}/>
-         <div><AddImagetoEdit /><SaveImage /></div>
+         <Image imgSource={this.state.url}/>
+         <div><AddImagetoEdit UpdateImage={this.UpdateImage} /><SaveImage /></div>
       </div>
     );
   }
